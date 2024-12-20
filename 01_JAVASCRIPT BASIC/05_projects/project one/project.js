19dd5e0b2f5d88bd341b60dc3a5493ecb8ac5ad0
shivtf <shivam714bhandari@gmail.com>
2024-12-20 11:23:15 +0530

    project 1

---
+++ 01_JAVASCRIPT BASIC/05_projects/project one/project.js
@@ -0,0 +1,27 @@
+const button = document.querySelectorAll('.button')
+const body = document.querySelector('body')
+
+// events
+
+button.forEach( (button) => {
+  console.log(button)
+  button.addEventListener('click',(e)=>{
+    console.log(e)
+    console.log(e.target)
+    if(e.target.id === 'red'){
+      body.style.backgroundColor = e.target.id
+    }
+    if(e.target.id === 'blue'){
+      body.style.backgroundColor = e.target.id
+    }
+    if(e.target.id === 'green'){
+      body.style.backgroundColor = e.target.id
+    }
+    if(e.target.id === 'yellow'){
+      body.style.backgroundColor = e.target.id
+    }
+    if(e.target.id === 'purple'){
+      body.style.backgroundColor = e.target.id
+    }
+  })
+} );
