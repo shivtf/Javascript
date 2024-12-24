3561072ca4b97f47702d9597d361d2ba8b64986b
shivtf <shivam714bhandari@gmail.com>
2024-12-24 08:46:13 +0530

    commit 8

---
+++ 01_JAVASCRIPT BASIC/05_projects/project five/project.js
@@ -0,0 +1,27 @@
+// genrate random color
+const randomColor = function(){
+  const hex = "0123456789ABCDEF"
+  let color = '#'
+  for(let i = 0;i < 6; i++){
+    color += hex[Math.floor(Math.random()*16)]
+  }
+  return color;
+};
+let intervalId;
+const startChangeColor = function(){
+  if(!intervalId){
+    intervalId = setInterval(changeBGColor,800);
+  }
+
+  function changeBGColor(){
+    document.body.style.backgroundColor = randomColor();
+
+  }
+};
+const stopChangeColor = function(){
+  clearInterval(intervalId);
+  //intervalId = null;
+};
+
+document.querySelector('#start').addEventListener('click',startChangeColor)
+document.querySelector('#stop').addEventListener('click',stopChangeColor)
